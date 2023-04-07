3431c4796de5c833b2070846afe61ba25bbf329c
Pranav Baraiya <59532169+prnvbaraiya@users.noreply.github.com>
2023-04-07 14:20:53 +0530

    Delete Backend/Backend directory

--- Backend/Backend/model/model.js
+++
@@ -1,6 +0,0 @@
-const UserModel = require("./UserModel.js");
-const QuizModel = require("./QuizModel.js");
-const QuizResultModel = require("./QuizResult");
-const UserRecordingModel = require("./UserRecording");
-
-module.exports = { UserModel, QuizModel, QuizResultModel, UserRecordingModel };
